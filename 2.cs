69c66f336840b00141605647ea123cfb05d8429a
Haryce <148765923+Haryce@users.noreply.github.com>
2025-02-27 01:07:43 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+class Trelugl
+{
+    public double w;
+    public double h;
+
+    public Trelugl(double new_w, double new_h)
+    {
+        w = new_w;
+        h = new_h;
+    }
+    public double Ploshad()
+    {
+        return w * h;
+    }
+    public double Perimeter()
+    {
+        return 2 * (w + h);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Trelugl trelugl = new Trelugl(5, 10);
+        Console.WriteLine($"S: {trelugl.Ploshad()}");
+        Console.WriteLine($"P: {trelugl.Perimeter()}");
+    }
+}
+}
